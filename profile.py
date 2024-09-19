a86fa10e49868263889a58f3a4f9a698563b420e
Ngo Gia Bao <tranbaoelie@gmail.com>
2024-09-18 23:14:59 -0400

    change 500gb and disk_image so that user can choose options for them

--- profile.py
+++ profile.py
@@ -16,6 +16,18 @@ pc.defineParameter( "corecount",
                    portal.ParameterType.INTEGER, 2 )
 pc.defineParameter( "ramsize", "MB of RAM in each node.  NB: Make certain your requested cluster can supply this quantity.", 
                    portal.ParameterType.INTEGER, 2048 )
+pc.defineParameter("storage", "GB of storage for each node",
+                   portal.ParameterType.INTEGER, 500)
+pc.defineParameter("disk_image", "Disk Image for each node",
+                   portal.ParameterType.Enum, 
+                   "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+                   [
+                      "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD",
+                      "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD",
+                      "urn:publicid:IDN+utah.cloudlab.us+image+emulab-ops:UBUNTU18-64-A-OSCP-T",
+                      "urn:publicid:IDN+utah.cloudlab.us+image+emulab-ops:UBUNTU22-64-ARM"
+                   ]
+                   )
 
 params = pc.bindParameters()
 
@@ -40,6 +52,8 @@ prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
 num_nodes = params.n
+storage = params.storage
+disk_image = params.disk_image
 
 def return_numnodes():
     return num_nodes
@@ -48,15 +62,15 @@ for i in range(num_nodes):
   if i == 0:
     node = request.XenVM("head")
     bs_landing = node.Blockstore("bs_image", "/image")
-    bs_landing.size = "500GB"
+    bs_landing.size = storage
   else:
     node = request.XenVM("worker-" + str(i))
   node.cores = params.corecount
   node.ram = params.ramsize
   bs_landing = node.Blockstore("bs_" + str(i), "/image")
-  bs_landing.size = "500GB"
+  bs_landing.size = storage
   node.routable_control_ip = "true" 
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+  node.disk_image = disk_image
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
